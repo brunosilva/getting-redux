4b432504ae439cd29658a65e59c433bd99650eba
Bruno Silva <brs.rsilva@gmail.com>
2021-10-13 18:43:37 -0300

    set product at cart

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
+// packages
 import React from "react";
+
+// redux
 import { Provider } from 'react-redux'
-import Catalog from './components/Catalog';
+
+// store redux
 import store from './store';
 
+// components
+import Catalog from './components/Catalog';
+import Cart from "./components/Cart";
+
 function App() {
-  return (
-    <Provider store={store}>
-      <Catalog />
-    </Provider>
-  );
+    return (
+        <Provider store={store}>
+            <Catalog />
+            <Cart />
+        </Provider>
+    );
 }
 
 export default App;
--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,13 +1,20 @@
 // packages
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
 // redux
+import { useDispatch } from "react-redux";
+
+// store redux
+import { addProductToCart } from "../store/modules/cart/actions";
+
+// types
 import { IProduct } from '../store/modules/cart/types';
 
 // services
 import api from "../services/api";
 
 const Catalog: React.FC = () => {
+    const dispatch = useDispatch();
     const [catalog, setCatalog] = useState<IProduct[]>([]);
 
     useEffect(() => {
@@ -16,6 +23,10 @@ const Catalog: React.FC = () => {
         })
     }, []);
 
+    const handleAddProductToCart = useCallback((product: IProduct) => {
+        dispatch(addProductToCart(product));
+    }, [dispatch]);
+
     return (
         <main>
             <h1>Catalog</h1>
@@ -23,7 +34,14 @@ const Catalog: React.FC = () => {
             {catalog.map(product => (
                 <article>
                     <strong>{product.title}</strong> {" - "}
-                    <span>{product.price}</span>
+                    <span>{product.price}</span> {" "}
+
+                    <button 
+                        type="button"
+                        onClick={() => handleAddProductToCart(product)}
+                    >
+                        Comprar
+                    </button>
                 </article>
             ))}
         </main>
